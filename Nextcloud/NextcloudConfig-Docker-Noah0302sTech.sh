fbc93b62bad8556827d1f7c00f26be5d4785dd39
Noah0302sTech <Noah0302sTech@gmail.com>
2023-02-27 16:59:30 +0100

    loading

--- Nextcloud/NextcloudConfig-Docker-Noah0302sTech.sh
+++ Nextcloud/NextcloudConfig-Docker-Noah0302sTech.sh
@@ -15,7 +15,17 @@
     while true; do
         read -p "Möchtest du die Docker-Container jetzt neustarten [empfohlen]? Y/N: " yn
         case $yn in
-            [Yy]* ) echo "Starte Docker-Container neu, bitte warten... " && sudo docker restart $(docker ps -q); break;;
+            [Yy]* ) echo "Starte Docker-Container neu, bitte warten... " && sudo docker restart $(docker ps -q) &> /dev/null &
+                    PID=$!
+                    i=1
+                    sp="/-\|"
+                    echo -n ' '
+                    while [ -d /proc/$PID ]
+                      do
+                      printf "\b${sp:i++%${#sp}:1}"
+                    done
+                    echo
+                    printf "\xE2\x9C\x94 \n"; break;;
             [Nn]* ) exit;;
             * ) echo "Bitte gib Y/y für Ja, oder N/n für Nein ein.";;
         esac
