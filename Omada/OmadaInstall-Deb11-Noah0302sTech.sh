613fda92a34122a1299e41d3793d689e822c22bc
Noah0302sTech <Noah0302sTech@gmail.com>
2023-03-01 12:22:58 +0100

    v1.5

--- Omada/OmadaInstall-Deb11-Noah0302sTech.sh
+++ Omada/OmadaInstall-Deb11-Noah0302sTech.sh
@@ -14,11 +14,17 @@
 
 #--- Install Java
   echo "Installiere OpenJDK-8, bitte warten... "
-  echo
 
-  sudo echo "deb http://deb.debian.org/debian/ sid main" | sudo tee -a /etc/apt/sources.list
-  sudo apt update && sudo apt install openjdk-8-jre-headless -y
-  sudo sed -i '\%^deb http://deb.debian.org/debian/ sid main%d' /etc/apt/sources.list
+  #--- Add sid main repo
+    sudo echo "deb http://deb.debian.org/debian/ sid main" | sudo tee -a /etc/apt/sources.list > /dev/null 2>&1
+
+  #--- Install OpenJDK-8
+    sudo apt update > /dev/null 2>&1
+    sudo apt install openjdk-8-jre-headless -y
+
+  #--- Remove sid main repo
+    sudo sed -i '\%^deb http://deb.debian.org/debian/ sid main%d' /etc/apt/sources.list > /dev/null 2>&1
+    echo
   
   echo
   echo
@@ -27,19 +33,23 @@
 
 #--- Install jsvc curl gnupg2
   echo "Installiere jsvc curl gnupg2, bitte warten... "
-  echo
 
-  sudo apt update && sudo apt install jsvc curl gnupg2 -y &> /dev/null &
-    PID=$!
-    i=1
-    sp="/-\|"
-    echo -n ' '
-    while [ -d /proc/$PID ]
-      do
-      printf "\b${sp:i++%${#sp}:1}"
-    done
+  #--- Refresh Packages
+    sudo apt update > /dev/null 2>&1
+
+  #--- Install jsvc curl gnupg2
+    sudo apt install jsvc curl gnupg2 -y &> /dev/null &
+      PID=$!
+      i=1
+      sp="/-\|"
+      echo -n ' '
+      while [ -d /proc/$PID ]
+        do
+        printf "\b${sp:i++%${#sp}:1}"
+      done
+      echo
+      printf "\xE2\x9C\x94 \n"
     echo
-    printf "\xE2\x9C\x94 \n"
   
   echo
   echo
@@ -48,49 +58,31 @@
 
 #--- Install MongoDB
   echo "Installiere MongoDB, bitte warten... "
-  echo
 
-  curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -  &> /dev/null &
-    PID=$!
-    i=1
-    sp="/-\|"
-    echo -n ' '
-    while [ -d /proc/$PID ]
-      do
-      printf "\b${sp:i++%${#sp}:1}"
-    done
-    echo
-    printf "\xE2\x9C\x94 \n"
-  echo
+  #--- Add apt key
+    curl -fsSL https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -  > /dev/null 2>&1
   
-  echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list &> /dev/null &
-    PID=$!
-    i=1
-    sp="/-\|"
-    echo -n ' '
-    while [ -d /proc/$PID ]
-      do
-      printf "\b${sp:i++%${#sp}:1}"
-    done
-    echo
-    printf "\xE2\x9C\x94 \n"
-  echo
+  #--- Configure sources.list
+    echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list > /dev/null 2>&1
 
-  sudo apt update && sudo apt install mongodb-org -y &> /dev/null &
-    PID=$!
-    i=1
-    sp="/-\|"
-    echo -n ' '
-    while [ -d /proc/$PID ]
-      do
-      printf "\b${sp:i++%${#sp}:1}"
-    done
+  #--- Install Mongo-DB
+    sudo apt update > /dev/null 2>&1
+    sudo apt install mongodb-org -y &> /dev/null &
+      PID=$!
+      i=1
+      sp="/-\|"
+      echo -n ' '
+      while [ -d /proc/$PID ]
+        do
+        printf "\b${sp:i++%${#sp}:1}"
+      done
+      echo
+      printf "\xE2\x9C\x94 \n"
     echo
-    printf "\xE2\x9C\x94 \n"
-  echo
 
-  sudo systemctl enable mongod --now
-  sudo systemctl status mongod
+  #--- Enable Mongo-DB
+    sudo systemctl enable mongod --now
+    sudo systemctl status mongod
   
   echo
   echo
@@ -113,9 +105,26 @@
     if [ -z "$omada_url" ]; then
       omada_url="https://static.tp-link.com/upload/software/2023/202301/20230130/Omada_SDN_Controller_v5.8.4_Linux_x64.deb"
     fi
-    wget "$omada_url"
+    echo
+
+    echo "Downloade Omada-Controller, bitte warten... "
+    wget "$omada_url" &> /dev/null &
+      PID=$!
+      i=1
+      sp="/-\|"
+      echo -n ' '
+      while [ -d /proc/$PID ]
+        do
+        printf "\b${sp:i++%${#sp}:1}"
+      done
+      echo
+      printf "\xE2\x9C\x94 \n"
+    echo
   
   #--- Installation
     echo "Installiere Omada-Controller, bitte warten... "
-    echo
+    
     sudo apt install ./Omada_SDN_Controller_*.deb
+
+    echo
+    echo
