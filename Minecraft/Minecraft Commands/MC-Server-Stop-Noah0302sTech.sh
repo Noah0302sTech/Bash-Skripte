54a25da7b75c656f7fcf8f7557e9ba32bb7b7d02
Noah0302sTech <Noah0302sTech@gmail.com>
2023-04-12 14:45:31 +0200

    v3.0.1

--- Minecraft/Minecraft Commands/MC-Server-Stop-Noah0302sTech.sh
+++ Minecraft/Minecraft Commands/MC-Server-Stop-Noah0302sTech.sh
@@ -3,25 +3,25 @@
 #   chmod +x MC-Server-Stop-Noah0302sTech.sh && sudo ./MC-Server-Stop-Noah0302sTech.sh
 
 #----- Check for administrative privileges
-  if [[ $EUID -ne 0 ]]; then
-    echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
-    exit 1
-  fi
+	if [[ $EUID -ne 0 ]]; then
+		echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+		exit 1
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    #--- Restart Server
-      sudo echo 'say Server wird in 5 Sekunden gestoppt...' > /run/minecraftserver.stdin
-      sudo echo 'save-all flush' > /run/minecraftserver.stdin
-      sleep 5
-      sudo echo 'stop' > /run/minecraftserver.stdin
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Service hat des Status: $status"
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Service hat des Status: $status"
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		#--- Restart Server
+		sudo echo 'say Server wird in 5 Sekunden gestoppt...' > /run/minecraftserver.stdin
+		sudo echo 'save-all flush' > /run/minecraftserver.stdin
+		sleep 5
+		sudo echo 'stop' > /run/minecraftserver.stdin
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Service hat des Status: $status"
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Service hat des Status: $status"
+	else
+		echo "Der Service hat des Status: $status"
+	fi
