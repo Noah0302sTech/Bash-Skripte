923ece2508ee58be541cf3f0c710ac2ec4aa394b
Noah0302sTech <Noah0302sTech@gmail.com>
2023-04-12 14:43:42 +0200

    v3.0.2

--- Minecraft/Minecraft Commands/MC-Host-Restart-Noah0302sTech.sh
+++ Minecraft/Minecraft Commands/MC-Host-Restart-Noah0302sTech.sh
@@ -3,91 +3,91 @@
 #   chmod +x MC-Host-Restart-Noah0302sTech.sh && sudo bash MC-Host-Restart-Noah0302sTech.sh
 
 #----- Check for administrative privileges
-  if [[ $EUID -ne 0 ]]; then
-    echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
-    exit 1
-  fi
+	if [[ $EUID -ne 0 ]]; then
+		echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+		exit 1
+	fi
 
 
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    #--- Restart Server
-      sudo echo 'say Server wird in 5 Sekunden gestoppt...' > /run/minecraftserver.stdin
-      sudo echo 'save-all flush' > /run/minecraftserver.stdin
-      sleep 5
-      sudo echo 'stop' > /run/minecraftserver.stdin
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		#--- Restart Server
+		sudo echo 'say Server wird in 5 Sekunden gestoppt...' > /run/minecraftserver.stdin
+		sudo echo 'save-all flush' > /run/minecraftserver.stdin
+		sleep 5
+		sudo echo 'stop' > /run/minecraftserver.stdin
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+	else
+		echo "Der Service hat des Status: $status"
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    sleep 5
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		sleep 5
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	else
+		echo "Der Service hat des Status: $status"
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    sleep 10
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		sleep 10
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	else
+		echo "Der Service hat des Status: $status"
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    sleep 15
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Minecraft Server hast den Status: $status"
-    echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
-    shutdown -r 0
-    exit
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		sleep 15
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Minecraft Server hast den Status: $status"
+		echo "Deswegen wird der Host in 5 Sekunder heruntergefahren!"
+		shutdown -r 0
+		exit
+	else
+		echo "Der Service hat des Status: $status"
+	fi
