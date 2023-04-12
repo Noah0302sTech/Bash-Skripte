aef31800f5edfa1bb6e6e44602a2d03b32c077db
Noah0302sTech <Noah0302sTech@gmail.com>
2023-04-12 14:45:11 +0200

    v2.5.1

--- Minecraft/Minecraft Commands/MC-Server-Restart-Noah0302sTech.sh
+++ Minecraft/Minecraft Commands/MC-Server-Restart-Noah0302sTech.sh
@@ -3,25 +3,25 @@
 #   chmod +x MC-Server-Restart-Noah0302sTech.sh && sudo ./MC-Server-Restart-Noah0302sTech.sh
 
 #----- Check for administrative privileges
-  if [[ $EUID -ne 0 ]]; then
-    echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
-    exit 1
-  fi
+	if [[ $EUID -ne 0 ]]; then
+		echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+		exit 1
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    #--- Restart Server
-      sudo echo 'say Server wird in 5 Sekunden neu gestartet...' > /run/minecraftserver.stdin
-      sudo echo 'save-all flush' > /run/minecraftserver.stdin
-      sleep 5
-      sudo systemctl restart minecraftserver.service
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Service hat des Status: $status"
-  elif [ "${status}" = "inactive" ]; then
-    echo "Der Service hat des Status: $status"
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		#--- Restart Server
+		sudo echo 'say Server wird in 5 Sekunden neu gestartet...' > /run/minecraftserver.stdin
+		sudo echo 'save-all flush' > /run/minecraftserver.stdin
+		sleep 5
+		sudo systemctl restart minecraftserver.service
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Service hat des Status: $status"
+	elif [ "${status}" = "inactive" ]; then
+		echo "Der Service hat des Status: $status"
+	else
+		echo "Der Service hat des Status: $status"
+	fi
--- Minecraft/Minecraft Commands/MC-Server-Start-Noah0302sTech.sh
+++ Minecraft/Minecraft Commands/MC-Server-Start-Noah0302sTech.sh
@@ -3,22 +3,22 @@
 #   chmod +x MC-Server-Start-Noah0302sTech.sh && sudo ./MC-Server-Start-Noah0302sTech.sh
 
 #----- Check for administrative privileges
-  if [[ $EUID -ne 0 ]]; then
-    echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
-    exit 1
-  fi
+	if [[ $EUID -ne 0 ]]; then
+		echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+		exit 1
+	fi
 
 
 
 #----- Check for MC-Status
-  status="$(systemctl is-active minecraftserver.service)"
-  if [ "${status}" = "active" ]; then
-    echo "Der Service hat des Status: $status"
-  elif [ "${status}" = "dead" ]; then
-    echo "Der Service hat des Status: $status"
-  elif [ "${status}" = "inactive" ]; then
-    #--- Restart Server
-      sudo systemctl restart minecraftserver.service
-  else
-    echo "Der Service hat des Status: $status"
-  fi
+	status="$(systemctl is-active minecraftserver.service)"
+	if [ "${status}" = "active" ]; then
+		echo "Der Service hat des Status: $status"
+	elif [ "${status}" = "dead" ]; then
+		echo "Der Service hat des Status: $status"
+	elif [ "${status}" = "inactive" ]; then
+		#--- Restart Server
+		sudo systemctl restart minecraftserver.service
+	else
+		echo "Der Service hat des Status: $status"
+	fi
