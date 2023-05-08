1edf2a2196e2e93f803e340f108d87401b465e4b
Noah0302sTech <Noah0302sTech@gmail.com>
2023-05-08 12:22:22 +0200

    v1.0.0

---
+++ Docker/Docker-System-Prune-Trim.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+# Made by Noah0302sTech
+# chmod +x Docker-System-Prune-Trim.sh && sudo bash Docker-System-Prune-Trim.sh
+
+#---------- Initial Checks & Functions
+	#----- Check for administrative privileges
+		if [[ $EUID -ne 0 ]]; then
+			echo "Das Skript muss mit Admin-Privilegien ausgeführt werden! (sudo)"
+			exit 1
+		fi
+
+
+
+	#----- Source of Spinner-Function: https://github.com/tlatsas/bash-spinner
+			function _spinner() {
+				# $1 start/stop
+				#
+				# on start: $2 display message
+				# on stop : $2 process exit status
+				#           $3 spinner function pid (supplied from stop_spinner)
+
+				local on_success="DONE"
+				local on_fail="FAIL"
+				local white="\e[1;37m"
+				local green="\e[1;32m"
+				local red="\e[1;31m"
+				local nc="\e[0m"
+
+				case $1 in
+					start)
+						# calculate the column where spinner and status msg will be displayed
+						let column=$(tput cols)-${#2}-8
+						# display message and position the cursor in $column column
+						echo -ne ${2}
+						printf "%${column}s"
+
+						# start spinner
+						i=1
+						sp='\|/-'
+						delay=${SPINNER_DELAY:-0.15}
+
+						while :
+						do
+							printf "\b${sp:i++%${#sp}:1}"
+							sleep $delay
+						done
+						;;
+					stop)
+						if [[ -z ${3} ]]; then
+							echo "spinner is not running.."
+							exit 1
+						fi
+
+						kill $3 > /dev/null 2>&1
+
+						# inform the user uppon success or failure
+						echo -en "\b["
+						if [[ $2 -eq 0 ]]; then
+							echo -en "${green}${on_success}${nc}"
+						else
+							echo -en "${red}${on_fail}${nc}"
+						fi
+						echo -e "]"
+						;;
+					*)
+						echo "invalid argument, try {start/stop}"
+						exit 1
+						;;
+				esac
+			}
+
+			function start_spinner {
+				# $1 : msg to display
+				_spinner "start" "${1}" &
+				# set global spinner pid
+				_sp_pid=$!
+				disown
+			}
+
+			function stop_spinner {
+				# $1 : command exit status
+				_spinner "stop" $1 $_sp_pid
+				unset _sp_pid
+			}
+
+
+
+	#----- Refresh Packages
+		start_spinner "Aktualisiere Package-Listen..."
+			sudo apt update -y > /dev/null 2>&1
+		stop_spinner $?
+		echo
+		echo
+
+#-----	-----#	#-----	-----#	#-----	-----#
+#-----	-----#	#-----	-----#	#-----	-----#
+#-----	-----#	#-----	-----#	#-----	-----#
+
+
+
+
+
+#----- APT
+	#--- Update
+		start_spinner "Update Package-Listen..."
+			apt update -y > /dev/null 2>&1
+		stop_spinner $?
+
+	#--- Autoremove
+		start_spinner "Autoremove Packages..."
+			apt autoremove -y > /dev/null 2>&1
+		stop_spinner $?
+	echo
+	echo
+
+
+
+#----- Variables
+	dockerPruneOutput="Docker-System-Prune did not run!"
+	fstrimOutput="FS-Trim did not run!"
+
+
+
+#----- Docker
+	if command -v docker &> /dev/null
+	then
+		if [[ -z "$(docker ps -q -f status=exited)" ]]; then
+			start_spinner "Alle Docker Container laufen, führe Docker-System-Prune aus..."
+				dockerPruneOutput=$(docker system prune -f 2>&1)
+			stop_spinner $?
+			echo $dockerPruneOutput
+		else
+			echo "Es wurden gestoppte Container gefunden:"
+			docker ps -f "status=exited"
+		fi	
+	else
+		echo "Docker ist nicht installiert, überspringe Docker System Prune"
+	fi
+	echo
+	echo
+
+
+
+#----- Trim
+	start_spinner "Trimme Filesystem..."
+		fstrimOutput=$(fstrim / -v 2>&1)
+	stop_spinner $?
+	echo $fstrimOutput
+	echo
+	echo
+
+
+
+
+
+#-----	-----#	#-----	-----#	#-----	-----# TO DO: check lsblk --discard
+#-----	-----#	#-----	-----#	#-----	-----#
+#-----	-----#	#-----	-----#	#-----	-----#
