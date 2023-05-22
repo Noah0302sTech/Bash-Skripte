3376e913a612f7fe61b7077383ad06864b6ae7e8
Noah0302sTech <Noah0302sTech@gmail.com>
2023-05-22 13:06:26 +0200

    v3.0.1

--- Proxmox/Update-Upgrade/Proxmox-UpdateUpgrade-Noah0302sTech.sh
+++ Proxmox/Update-Upgrade/Proxmox-UpdateUpgrade-Noah0302sTech.sh
@@ -3,9 +3,3 @@
 
 #Update
 	upgradeOutput=$(apt-get update && apt-get dist-upgrade -y 2>&1)
-
-#Debug
-	echo "Proxmox-Updater Cron-Job ran @" >> /root/Noah0302sTech/$folderVar/Cron-Debug.txt
-	date >> /root/Noah0302sTech/$folderVar/Cron-Debug.txt 
-	echo $upgradeOutput >> /root/Noah0302sTech/$folderVar/Cron-Debug.txt 
-	echo '' >> /root/Noah0302sTech/$folderVar/Cron-Debug.txt" > /root/Proxmox-UpdateUpgrade-Noah0302sTech.sh
