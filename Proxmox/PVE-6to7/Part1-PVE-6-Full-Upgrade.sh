45e28a8b6aa65fb56f1556388166696957763cb9
Noah0302sTech <Noah0302sTech@gmail.com>
2023-06-19 12:51:26 +0200

    v1.0.0
    Tested with 3x PVE 6.4-4 VMs running Ceph 14
    0 Downtime achieved by moving VMs from the currently upgrading Node

---
+++ Proxmox/PVE-6to7/Part1-PVE-6-Full-Upgrade.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#	Made by Noah0302sTech
+# 	chmod +x Part1-PVE-6-Full-Upgrade.sh && bash Part1-PVE-6-Full-Upgrade.sh
+
+
+
+#Create Folder
+	mkdir PVE-6to7
+
+	echo
+	echo
+	echo
+
+#Move Bash-Script
+	mv Part1-PVE-6-Full-Upgrade.sh PVE-6to7/Part1-PVE-6-Full-Upgrade.sh
+
+	echo
+	echo
+	echo
+
+#Delete Enterprise Repo
+	echo "----- Delete Enterprise Repo -----"
+	rm /etc/apt/sources.list.d/pve-enterprise.list
+
+	echo
+	echo
+	echo
+
+#Add No-Subscription Repo
+	echo "----- Add No-Subscription Repo -----"
+	echo "
+deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list
+
+	echo
+	echo
+	echo
+
+#Update to latest 6.4-X
+	echo "----- Update to latest 6.4-X -----"
+	apt update && apt upgrade
+
+	echo
+	echo
+	echo
+
+#Reboot Node
+	echo "----- Reboot Node -----"
+	echo "ACHTUNG, erst Node rebooten, wenn keine VM mehr auf diesem Node läuft!"
+	while IFS= read -n1 -r -p "Jetzt Node rebooten? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+	case $REPLY in
+		y)  echo
+
+			echo "Starte in 15 Sekunden neu!"
+			echo
+			echo
+			echo
+			sleep 15
+			reboot
+
+			break;;
+		n)  echo
+			echo "Node wurde nicht rebotet! Neue Kernel-Version noch nicht aktiv!"
+			
+			break;;
+		*)  echo
+			echo "Antoworte mit y oder n";;
+	esac
+	done
+
+	echo
+	echo
+	echo
---
+++ Proxmox/PVE-6to7/Part2-Ceph-14to15.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+#	Made by Noah0302sTech
+# 	chmod +x Part2-Ceph-14to15.sh && bash Part2-Ceph-14to15.sh
+
+
+
+#Move Bash-Script
+	mv Part2-Ceph-14to15.sh PVE-6to7/Part2-Ceph-14to15.sh
+
+	echo
+	echo
+	echo
+
+#Update to latest 6.4-X
+	echo "----- Update to latest 6.4-X -----"
+	apt update && apt upgrade
+
+	echo
+	echo
+	echo
+
+#Change the current Ceph repositories from Nautilus to Octopus
+	echo "----- Change the current Ceph repositories from Nautilus to Octopus -----"
+	sed -i 's/nautilus/octopus/' /etc/apt/sources.list.d/ceph.list
+
+	echo
+	echo
+	echo
+
+#Set the noout flag for the duration of the upgrade (optional, but recommended)
+	echo "----- Set the noout flag for the duration of the upgrade (optional, but recommended) -----"
+	ceph osd set noout
+
+	echo
+	echo
+	echo
+
+#Upgrade on each Ceph cluster node
+	echo "----- Upgrade on each Ceph cluster node -----"
+	apt update && apt full-upgrade
+	
+	echo
+	echo
+	echo
+
+#Restart the monitor daemon
+	echo "----- Restart the monitor daemon -----"
+	systemctl restart ceph-mon.target
+
+	echo
+	echo
+	echo
+
+#Restart the manager daemon
+	echo "----- Restart the manager daemon -----"
+	systemctl restart ceph-mgr.target
+
+	echo
+	echo
+	echo
+
+#Restart the OSD daemon
+	echo "----- Restart the OSD daemon-----"
+	systemctl restart ceph-osd.target
+
+	echo
+	echo
+	echo
+
+#Status Ceph
+	echo "----- Status Ceph -----"
+	ceph status
+
+	echo
+	echo
+	echo
+
+#Restart the Meta-Data-Servers
+	echo "ACHTUNG, die MDS sollten erst neu gestartet werden, wenn alle OSDs des HOSTs geupgraded sind!"
+	while IFS= read -n1 -r -p "Jetzt die Meta-Data-Server neu starten? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+	case $REPLY in
+		y)  echo
+			systemctl restart ceph-mds.target
+
+			break;;
+		n)  echo
+			echo "Meta-Data-Server wurden nicht neu gestartet!"
+			echo "Falls du das später machen möchtest, gebe folgenden Befehl ein:"
+			echo "systemctl restart ceph-mds.target"
+			
+			break;;
+		*)  echo
+			echo "Antoworte mit y oder n";;
+	esac
+	done
+
+	echo
+	echo
+	echo
+
+#Info Disallow pre-Octopus OSDs and enable all new Octopus-only functionality
+	echo "ACHTUNG, allow Octopus-only functionality sollten erst aktiviert werden, wenn ALLE OSDs des CLUSTERs geupgraded sind!"
+	while IFS= read -n1 -r -p "Jetzt Octopus-only functionality aktivieren? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+	case $REPLY in
+		y)  echo
+			ceph osd require-osd-release octopus
+
+			break;;
+		n)  echo
+			echo "Octopus-only functionality wurde nicht aktiviert!"
+			echo "Falls du das später machen möchtest, gebe folgenden Befehl ein:"
+			echo "ceph osd require-osd-release octopus"
+			
+			break;;
+		*)  echo
+			echo "Antoworte mit y oder n";;
+	esac
+	done
+
+	echo
+	echo
+	echo
+
+#Reboot Node
+	echo "----- Reboot Node -----"
+	echo "ACHTUNG, erst Node rebooten, wenn keine VM mehr auf diesem Node läuft!"
+	while IFS= read -n1 -r -p "Jetzt Node rebooten? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+	case $REPLY in
+		y)  echo
+
+			echo "Starte in 15 Sekunden neu!"
+			echo
+			echo
+			echo
+			sleep 15
+			reboot
+
+			break;;
+		n)  echo
+			echo "Node wurde nicht rebotet!"
+			
+			break;;
+		*)  echo
+			echo "Antoworte mit y oder n";;
+	esac
+	done
+
+	echo
+	echo
+	echo
