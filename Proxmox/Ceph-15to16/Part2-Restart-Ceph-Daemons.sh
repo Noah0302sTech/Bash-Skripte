83f9b631bbf3d3380ce28723cc93668284ac17f5
Noah0302sTech <Noah0302sTech@gmail.com>
2023-06-20 16:03:46 +0200

    Added everything up to Restart OSDs

--- Proxmox/Ceph-15to16/Part2-Restart-Ceph-Daemons.sh
+++ Proxmox/Ceph-15to16/Part2-Restart-Ceph-Daemons.sh
@@ -24,4 +24,54 @@
 
 
 
-#
+#Restart the monitor daemon
+	echo "----- Restart the monitor daemon -----"
+		systemctl restart ceph-mon.target
+
+	echoEnd
+
+
+
+#Restart the manager daemons on all nodes
+	echo "ACHTUNG, erst Ceph-Manager-Daemons neu starten, wenn alle Ceph-Monitore geupgraded sind! [min_mon_release 16 (pacific)]"
+		ceph mon dump | grep min_mon_release
+		while IFS= read -n1 -r -p "Sind alle Ceph-Monitore geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+				systemctl restart ceph-mgr.target
+
+				break;;
+			n)  echo
+				sleep 5
+				ceph mon dump | grep min_mon_release;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Restart the OSD daemon on all nodes
+	echo "ACHTUNG, erst alle Ceph-OSDs neu starten, wenn alle Ceph-Manager geupgraded sind! [HEALTH_OK]"
+		ceph status
+		while IFS= read -n1 -r -p "Sind alle Ceph-Monitore geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+				systemctl restart ceph-osd.target
+
+				break;;
+			n)  echo
+				sleep 5
+				ceph status;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
