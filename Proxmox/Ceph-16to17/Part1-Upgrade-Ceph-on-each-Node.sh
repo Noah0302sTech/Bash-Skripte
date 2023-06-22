6d9c9b2ff9d17f42f7f6ab1b746f7c4671b2c479
Noah0302sTech <Noah0302sTech@gmail.com>
2023-06-22 16:22:11 +0200

    v1.0.0
    Merge from Testing - See Testing Branche for further Info

---
+++ Proxmox/Ceph-16to17/Part1-Upgrade-Ceph-on-each-Node.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+#	Made by Noah0302sTech
+# 	chmod +x Part1-Upgrade-Ceph-on-each-Node.sh && bash Part1-Upgrade-Ceph-on-each-Node.sh
+#	wget https://raw.githubusercontent.com/Noah0302sTech/Bash-Skripte/testing/Proxmox/Ceph-16to17/Part1-Upgrade-Ceph-on-each-Node.sh && bash Part1-Upgrade-Ceph-on-each-Node.sh
+
+
+
+#----------- Init
+	#----- echoEnd
+		function echoEnd {
+			echo
+			echo
+			echo
+		}
+
+	#----- Variables
+		url="https://raw.githubusercontent.com/Noah0302sTech/Bash-Skripte/testing/Proxmox/Ceph-16to17/Part2-Restart-Ceph-Daemons.sh"
+
+
+
+#Move Bash-Script
+	mkdir Ceph-16to17
+	mv Part1-Upgrade-Ceph-on-each-Node.sh Ceph-16to17/Part1-Upgrade-Ceph-on-each-Node.sh
+
+	echoEnd
+
+
+
+#Update to latest 7.X-X
+	echo "----- Update to latest 7.X-X -----"
+		apt update && apt dist-upgrade
+
+	echoEnd
+
+
+
+#Enable msgrv2 protocol and update Ceph configuration
+	echo "----- Enable msgrv2 protocol -----"
+		ceph mon enable-msgr2
+		ceph mon dump
+		echo
+
+		echo "Alle Ceph-Monitore MÜSSEN ein v1 UND eine v2 Addresse haben!"
+		while IFS= read -n1 -r -p "Sind v1 UND v2 verfügbar? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5
+				ceph mon dump
+				echo;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Change the current Ceph repositories from Pacific to Quincy
+	echo "----- Change the current Ceph repositories from Pacific to Quincy -----"
+		sed -i 's/pacific/quincy/' /etc/apt/sources.list.d/ceph.list
+
+	echoEnd
+
+
+
+#Set the noout flag for the duration of the upgrade (optional, but recommended)
+	echo "----- Set the noout flag for the duration of the upgrade (optional, but recommended) -----"
+		ceph osd set noout
+
+	echoEnd
+
+
+
+#Upgrade on each Ceph cluster node
+	echo "----- Upgrade on each Ceph cluster node -----"
+		apt update && apt full-upgrade
+
+	echoEnd
+
+
+
+#Continue with Part2
+	echo "ACHTUNG, erst bei Part2 weitermachen, wenn alle Nodes geupgraded sind!"
+		while IFS= read -n1 -r -p "Sind alle Nodes geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+				wget $url
+				bash Part2-Restart-Ceph-Daemons.sh
+
+				break;;
+			n)  echo
+				echo "Part 2 wurde noch nicht ausgeführt!"
+				
+				break;;
+			*)  echo
+				echo "Antoworte mit y oder n";;
+
+		esac
+		done
+
+	echoEnd
---
+++ Proxmox/Ceph-16to17/Part2-Restart-Ceph-Daemons.sh
@@ -0,0 +1,182 @@
+#!/bin/bash
+#	Made by Noah0302sTech
+# 	chmod +x Part2-Restart-Ceph-Daemons.sh && bash Part2-Restart-Ceph-Daemons.sh
+#	wget https://raw.githubusercontent.com/Noah0302sTech/Bash-Skripte/testing/Proxmox/Ceph-16to17/Part2-Restart-Ceph-Daemons.sh && bash Part2-Restart-Ceph-Daemons.sh
+
+#TODO:	Alles ab Ceph-OSDs neu starten überprüfen
+
+#----------- Init
+	#----- echoEnd
+		function echoEnd {
+			echo
+			echo
+			echo
+		}
+
+	#----- Variables
+		url="XXXXXXXXXX"
+
+
+
+#Move Bash-Script
+	mv Part2-Restart-Ceph-Daemons.sh Ceph-16to17/Part2-Restart-Ceph-Daemons.sh
+
+	echoEnd
+
+
+
+#Restart the monitor daemon
+	echo "----- Restart the monitor daemon -----"
+		systemctl restart ceph-mon.target
+
+	echoEnd
+
+
+
+#Restart the manager daemons on all nodes
+	echo "ACHTUNG, erst Ceph-Manager-Daemons neu starten, wenn alle Ceph-Monitore geupgraded sind! [min_mon_release 17 (quincy)]"
+		ceph mon dump | grep min_mon_release
+		while IFS= read -n1 -r -p "Sind alle Ceph-Monitore geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+
+				systemctl restart ceph-mgr.target
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5
+
+				ceph mon dump | grep min_mon_release
+				echo
+
+				echo "Falls sie nicht geupgraded sind, kann das auch per 'systemctl restart ceph-mon.target' auf jedem Node gemacht werden!"
+				echo;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Restart the OSD daemon on all nodes
+	echo "ACHTUNG, erst alle Ceph-OSDs neu starten, wenn alle Ceph-Manager geupgraded sind! In der GUI sichtbar!"
+		while IFS= read -n1 -r -p "Sind alle Ceph-Manager geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+
+				systemctl restart ceph-osd.target
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5
+
+				echo
+
+				echo "Falls sie nicht geupgraded sind, kann das auch per 'systemctl restart ceph-mgr.target' auf jedem Node gemacht werden!"
+				echo;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Disallow pre-Quincy OSDs and enable all new Quincy-only functionality
+	echo "ACHTUNG, allow Quincy-only functionality sollten erst aktiviert werden, wenn ALLE OSDs des CLUSTERs geupgraded sind!"
+		ceph tell osd.* version
+		while IFS= read -n1 -r -p "Sind alle Ceph-OSDs geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+
+				ceph osd require-osd-release quincy
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5
+				
+				ceph tell osd.* version
+				echo
+
+				echo "Falls sie nicht geupgraded sind, kann das auch per 'systemctl restart ceph-osd.target' auf jedem Node gemacht werden!"
+				echo;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Upgrade all CephFS MDS daemons
+	echo "ACHTUNG, die CephFS-MDS sollten erst geupgraded werden, wenn ALLE non-zero ranks deaktiviert sind!"
+		while IFS= read -n1 -r -p "Sind ALLE non-zero ranks deaktiviert [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+
+				systemctl restart ceph-mds.target
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5
+
+				echo
+
+				echo "Falls sie nicht geupgraded sind, kann das auch per 'systemctl restart ceph-mds.target' auf jedem Node gemacht werden!"
+				echo;;
+
+			*)  echo
+				echo "Antoworte mit y oder n";;
+				
+		esac
+		done
+
+	echoEnd
+
+
+
+#Unset the 'noout' flag
+	echo "ACHTUNG, noout flag erst deaktivieren, wenn ALLES fertig ist!"
+		while IFS= read -n1 -r -p "Sind alle Nodes geupgraded? [y]es|[n]o: " && [[ $REPLY != q ]]; do
+		case $REPLY in
+			y)  echo
+			
+				ceph osd unset noout
+
+				break;;
+
+			n)  echo
+
+				echo "Warte 5 weitere Sekunden..."
+				sleep 5;;
+				
+				
+			*)  echo
+				echo "Antoworte mit y oder n";;
+
+		esac
+		done
+
+	echoEnd
