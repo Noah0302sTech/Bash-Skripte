a900082fb2bcf3190f97856d616abaaf8efe181a
Noah0302sTech <Noah0302sTech@gmail.com>
2023-07-04 14:49:18 +0200

    Merged Check for FSTrim from Master

--- Docker/System-Prune/System-Prune-Executer/Docker-SystemPrune-Executer-Noah0302sTech.sh
+++ Docker/System-Prune/System-Prune-Executer/Docker-SystemPrune-Executer-Noah0302sTech.sh
@@ -149,32 +149,35 @@
 #----- Check if the filesystem supports fstrim command
 	#--- Check if the script is running within a container
 	if [ -f /proc/1/environ ] && grep -q container=lxc /proc/1/environ; then
-		echo "Script is running within a container!"
-		echo "You might not have Permissions to use the 'fstrim' Command!"
-		echo "Script will be aborted!"
+		echo "Script läuft in einem Container!"
+		echo "Möglicherweise keine Berechtigungen für 'fstrim' Command!"
+		echo "Script wird abgebrochen!"
 		exit 0
 	fi
 
 	#--- Check if the script is running within a VMware VM
 	if command -v dmidecode >/dev/null 2>&1 && dmidecode -s system-product-name | grep -qi "VMware"; then
-		echo "Script is running within a VMware VM"
-		echo "You might not have Permissions to use the 'fstrim' Command!"
-		echo "Script will be aborted!"
+		echo "Script läuft in einer VMware-VM"
+		echo "Möglicherweise keine Berechtigungen für 'fstrim' Command"
+		echo "Script wird abgebrochen!"
 		exit 0
 	fi
 
 	#--- Check if the filesystem supports fstrim command
 	if blkid -o value -s discard $(findmnt -n -o SOURCE --target /) >/dev/null 2>&1; then
-		echo "Filesystem supports fstrim command"
+		echo "Filesystem unterstützt 'fstrim' Command"
 
 		# Run fstrim on the root filesystem
 		if command -v fstrim >/dev/null 2>&1; then
-			dockerPruneOutput=$(fstrim -v / 2>&1)
+			start_spinner "Trimme Filesystem..."
+				fstrimOutput=$(/sbin/fstrim -av 2>&1)
+			stop_spinner $?
+			echo $fstrimOutput
 		else
-			echo "fstrim command is not available"
+			echo "'fstrim' Command ist nicht verfügbar"
 		fi
 	else
-		echo "Filesystem does not support fstrim command"
+		echo "Filesystem unterstützt 'fstrim' Command NICHT!"
 	fi
 
 	echoEnd
